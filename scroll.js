a3b375b08a2fc92568fbd60c2b35c636b64fe9ce
Evgenii Kucheriavyi <e.kucheriavii@gmail.com>
2019-11-07 16:52:46 +0300

    Added more features

--- scroll.js
+++ scroll.js
@@ -1,10 +1,49 @@
 var progressBar = document.getElementById("progress__bar");
+var topButton = document.getElementById("top");
 
-window.addEventListener("scroll", function (e) { Scroll(e); });
+var timer = null;
+
+window.addEventListener("scroll", function (e) { Scroll(e); CheckScroll(e); });
+topButton.addEventListener("click", function (e) { ScrollToTop(400); });
 
 function Scroll(e)
 {
 	var percent = window.scrollY / window.scrollMaxY * 100;
 
+	console.log(window.scrollY);
+
 	progressBar.setAttribute("style", "width: " + percent + "%;");	
+}
+
+function CheckScroll(e)
+{
+	if(window.scrollY > 150)
+	{
+		topButton.className = "top";
+	}
+	else
+	{
+		topButton.className = "top top_hidden";
+	}
+}
+
+function ScrollToTop(length)
+{
+	var updateRate = 1000 / 60;
+
+	var frames = length / updateRate;
+
+	var val = window.scrollY / frames;
+
+	timer = setInterval(function (e) { UpdateScroll(val); }, updateRate);
+}
+
+function UpdateScroll(val)
+{
+	window.scroll(0, window.scrollY - val);
+	
+	if(window.scrollY <= 0)
+	{
+		clearInterval(timer);
+	}
 }
